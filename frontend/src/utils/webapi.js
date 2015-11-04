301e9adefc55055bf8123795295b985f636722c4
Saketh Kasibatla <sake.kasi@gmail.com>
2015-11-03 22:49:02 -0800

    working on call to maintain cache of piazza data in webapi

--- frontend/src/utils/webapi.js
+++ frontend/src/utils/webapi.js
@@ -14,9 +14,12 @@ import Node from '../Models/node.js';
 var courseId = "42";
 var mainUrl = "";
 
-// TODO(saketh): This is a hardcoded user id. change this dynamically during
+// TODO(nate): This is a hardcoded user id. change this dynamically during
 // runtime based on which user is actually viewing
 var userId = 1;
+
+//TODO: this is hardcoded. fix plz
+var classId = "if44ov1fn5a505";
 
 function getNode(nodeId: string){
   let endpoint = mainUrl + `/${courseId}/node/${nodeId}/`;
@@ -106,6 +109,8 @@ export function addNewChild(node: Node, tag: string, markdown: string, renderer:
     });
 }
 
+var piazza_class;
+
 export function init(callback: any){
   let tree;
   getTree().then((data) => {
@@ -120,6 +125,41 @@ export function init(callback: any){
     console.error(textStatus);
     throw errorThrown;
   }).then((user) => {
+    piazza_class = user.getClassById(classId);
     callback(tree, user);
+  }).catch(function(error){
+    console.log(error);
+    throw error;
   });
+}
+
+var folderData = null;
+
+export function applyTimeFilter({start, end}){ //may want to make this asynchronous
+  if(!folderData){
+    folderData = {};
+
+    Promise.all(piazza_class.folders.map(piazza_class.filterByFolder.bind(piazza_class)))
+      .then(function(foldersFeedItems){
+        foldersFeedItems.forEach((feedItems, i) => {
+          folderData[piazza_class.folders[i]] = feedItems;
+        });
+      }).catch(function(error){
+        console.log(error);
+        throw error;
+      });
+  }
+
+  let filtered = [];
+  let startDate = Date.parse(start);
+  let endDate = Date.parse(end);
+
+  for( folder in folderData ){
+    filtered = filtered.concat(folderData[folder].filter(item => {
+      let date = Date.parse(item.lastModified);
+      return start <= date && date <= end;
+    });
+  }
+
+  return filtered;
 }
