8d3486998cb2daa4c29da0555002764921f1be3c
Saketh Kasibatla <sake.kasi@gmail.com>
2015-11-09 12:39:43 -0800

    updated directory structure to move frontend inside backend, removing need for symlink

--- backend/cdApp.py
+++ backend/cdApp.py
@@ -1,6 +1,6 @@
 import sqlite3
 from flask import Flask, request, g, render_template, \
-    jsonify, Response
+    jsonify, Response, send_from_directory
 from flask_restful import Resource, Api
 import json
 from piazza_api import Piazza
@@ -15,12 +15,7 @@ USERNAME = 'admin'
 PASSWORD = 'default'
 USER_FILE = 'sample_user.txt'
 
-if os.name == 'posix':
-    STATIC_PATH = '/static'
-else:
-    STATIC_PATH = '/staticwin'
-
-app = Flask(__name__, static_url_path=STATIC_PATH)
+app = Flask(__name__, static_folder='frontend')
 app.config.from_object(__name__)
 api = Api(app)
 
@@ -270,6 +265,9 @@ class Course(Resource):
 def posterator():
     return render_template('posterator.html')
 
+@app.route('/', methods=['GET'])
+def index():
+    return send_from_directory('frontend','index.html');
 
 api.add_resource(Node, '/<course_id>/node/<operation>/', '/<course_id>/node/<operation>/<node_id>/')
 # api.add_resource(Children, '/children/<operation>/<node_id>/')
