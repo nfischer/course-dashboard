4f4fca810101ca1b9b720ebe324c67788bd35dc7
drudiger <dustin.rudiger@ucla.edu>
2015-11-29 19:15:59 -0800

    Everything working but selenium

--- backend/frontend/test/selenium-test.js
+++ backend/frontend/test/selenium-test.js
@@ -4,36 +4,52 @@ var assert = require('chai').assert;
 var expect = require('chai').expect;
 
 before(function() {
-  this.timeout(20000);
+  this.timeout(10000);
   var childProcess = require('child_process');
-  var setup;
+ /*   var setup;
   var addData;
+  var cd;
 
-  setup = childProcess.exec('../setup.sh', function (error, stdout, stderr) {
+  cd = childProcess.exec('cd ..', function (error, stdout, stderr) {
     if (error) {
       console.log(error.stack);
       console.log('Error code: '+error.code);
       console.log('Signal received: '+error.signal);
     }
-    console.log('setup Child Process STDERR: '+stderr);
+
+    setup = childProcess.exec('./setup.sh', function (error, stdout, stderr) {
+      if (error) {
+        console.log(error.stack);
+        console.log('Error code: '+error.code);
+        console.log('Signal received: '+error.signal);
+      }
+      console.log('setup child process STDERR: '+stderr);
+    });
+
+    setup.on('exit', function (code) {
+      console.log('setup child process exited with exit code '+code);
+    });
+
+    addData = childProcess.exec('./addSampleData.py', function (error, stdout, stderr) {
+      if (error) {
+        console.log(error.stack);
+        console.log('Error code: '+error.code);
+        console.log('Signal received: '+error.signal);
+      }
+      console.log('addData child process STDERR: '+stderr);
+    });
+
+    addData.on('exit', function (code) {
+      console.log('addData child process exited with exit code '+code);
+    });
+
+    console.log('cd child process STDERR: '+stderr);
   });
 
-  setup.on('exit', function (code) {
-    console.log('Child process exited with exit code '+code);
-  });
+  cd.on('exit', function (code) {
+    console.log('cd child process exited with exit code '+code);
+  });*/
 
-  addData = childProcess.exec('../addSampleData.py', function (error, stdout, stderr) {
-    if (error) {
-      console.log(error.stack);
-      console.log('Error code: '+error.code);
-      console.log('Signal received: '+error.signal);
-    }
-    console.log('addData Child Process STDERR: '+stderr);
-  });
-
-  addData.on('exit', function (code) {
-    console.log('addData Child process exited with exit code '+code);
-  });
 
   this.driver = new selenium.Builder().withCapabilities(selenium.Capabilities.chrome()).build();
   return this.driver.getWindowHandle();
@@ -45,7 +61,7 @@ after(function() {
 
 describe('Landing page test', function() {
   beforeEach(function() {
-    return this.driver.get('127.0.0.1:5000/');
+    return this.driver.get('http://127.0.0.1:5000/');
   });
   it('has the project name as the title', function() {
     return expect(this.driver.getTitle()).to.eventually.contain('Course Dashboard');
@@ -55,7 +71,7 @@ describe('Landing page test', function() {
     this.driver.findElement({
       linkText: 'CS130'
     }).click();
-    return expect(this.driver.getCurrentUrl()).to.eventually.equal('127.0.0.1:5000/1/');
+    return expect(this.driver.getCurrentUrl()).to.eventually.equal('http://127.0.0.1:5000/1/');
   });
 
   // Example function
