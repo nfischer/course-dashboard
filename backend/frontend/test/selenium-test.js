3b7546272897b3e9799d908a891a429704aca0d2
drudiger <dustin.rudiger@ucla.edu>
2015-11-29 20:02:39 -0800

    Minor changes, updated package.json from master

--- backend/frontend/test/selenium-test.js
+++ backend/frontend/test/selenium-test.js
@@ -5,52 +5,6 @@ var expect = require('chai').expect;
 
 before(function() {
   this.timeout(10000);
-  var childProcess = require('child_process');
- /*   var setup;
-  var addData;
-  var cd;
-
-  cd = childProcess.exec('cd ..', function (error, stdout, stderr) {
-    if (error) {
-      console.log(error.stack);
-      console.log('Error code: '+error.code);
-      console.log('Signal received: '+error.signal);
-    }
-
-    setup = childProcess.exec('./setup.sh', function (error, stdout, stderr) {
-      if (error) {
-        console.log(error.stack);
-        console.log('Error code: '+error.code);
-        console.log('Signal received: '+error.signal);
-      }
-      console.log('setup child process STDERR: '+stderr);
-    });
-
-    setup.on('exit', function (code) {
-      console.log('setup child process exited with exit code '+code);
-    });
-
-    addData = childProcess.exec('./addSampleData.py', function (error, stdout, stderr) {
-      if (error) {
-        console.log(error.stack);
-        console.log('Error code: '+error.code);
-        console.log('Signal received: '+error.signal);
-      }
-      console.log('addData child process STDERR: '+stderr);
-    });
-
-    addData.on('exit', function (code) {
-      console.log('addData child process exited with exit code '+code);
-    });
-
-    console.log('cd child process STDERR: '+stderr);
-  });
-
-  cd.on('exit', function (code) {
-    console.log('cd child process exited with exit code '+code);
-  });*/
-
-
   this.driver = new selenium.Builder().withCapabilities(selenium.Capabilities.chrome()).build();
   return this.driver.getWindowHandle();
 });
